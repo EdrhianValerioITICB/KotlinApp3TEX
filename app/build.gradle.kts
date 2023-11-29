c8a873b1bc1ba2d3cab0666d01a825e1ac42566d
MustaphaBZ <2023_mustapha.bouleili@iticbcn.cat>
2023-11-29 18:38:17 +0100

    Colors and drawables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.edrhian.kotlinapp3tex"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.edrhian.kotlinapp3tex"
