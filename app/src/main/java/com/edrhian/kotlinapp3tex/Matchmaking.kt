00dd20d44ef89cdc82c074eaeb8436b43402c094
Edrhian Valerio <edrhian27@gmail.com>
2023-12-10 00:53:53 +0100

    Fix typos, crashes, and size of views

--- app/src/main/java/com/edrhian/kotlinapp3tex/Matchmaking.kt
+++ app/src/main/java/com/edrhian/kotlinapp3tex/Matchmaking.kt
@@ -1,12 +1,15 @@
 package com.edrhian.kotlinapp3tex
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class Matchmaking : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_matchmaking)
-        
+    }
+    fun toBack(view: View) {
+        super.onBackPressed()
     }
 }
