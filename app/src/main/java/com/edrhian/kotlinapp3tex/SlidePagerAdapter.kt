7b3bf53c2cd02a608f34158a6fa473795132f240
Edrhian Valerio <edrhian27@gmail.com>
2024-01-29 09:55:57 +0100

    Upload SlidePagerAdapter for tutorial part

---
+++ app/src/main/java/com/edrhian/kotlinapp3tex/SlidePagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.edrhian.kotlinapp3tex
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class SlidePagerAdapter(fm: FragmentManager?, private val fragmentList: List<Fragment>) :
+        FragmentStatePagerAdapter(
+                fm!!
+        ) {
+        override fun getItem(position: Int): Fragment {
+                return fragmentList[position]
+        }
+
+        override fun getCount(): Int {
+                return fragmentList.size
+        }
+}
