99485e3c9560ee994c76af57c8faad9fd81f06bb
Edrhian Valerio <edrhian27@gmail.com>
2024-02-25 10:32:46 +0100

    Add logo animation in PrimeraVista

--- app/src/main/java/com/edrhian/kotlinapp3tex/PrimeraVista.kt
+++ app/src/main/java/com/edrhian/kotlinapp3tex/PrimeraVista.kt
@@ -3,13 +3,21 @@ package com.edrhian.kotlinapp3tex
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentTransaction
 
 class PrimeraVista : AppCompatActivity() {
+    private lateinit var logo : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.primera_vista)
+        logo = findViewById(R.id.iv_logo)
+        val animacion = AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+        logo.startAnimation(animacion)
+
     }
 
     fun toLogin(view: View) {
